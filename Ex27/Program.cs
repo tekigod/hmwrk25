a84b8e6319fc3a84402e1a76805f21954a145cba
bladeshape <tekigod@mail.ru>
2022-08-23 05:46:27 +0300

    exd27

---
+++ Ex27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. выдаёт сумму цифр в числе.
+// Например: 
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using System;  
+  public class Zadanie27  
+   {  
+     public static void Main(string[] args)  
+      {  
+       int  n,sum=0,m;         
+       Console.Write("Введите число: ");      
+       n= int.Parse(Console.ReadLine());     
+       while(n>0)      
+       {      
+        m = n % 10;      
+        sum = sum + m;      
+        n = n / 10;      
+       }      
+       Console.Write("Сумма цифр заданного числа = "+sum);       
+     }  
+  }  
